a7c2aaa3d86e25f0de32c9a56a6e070c963f2961
slava <slava@ukr.net>
2022-07-28 14:29:45 +0300

    Movie Movies

---
+++ src/components/Movie.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function Movie(props) {
+    const {
+        Title: title,
+        Year: year,
+        imdbID: id,
+        Type: type,
+        Poster: poster
+    } = props;
+    return (
+
+        <div className="card">
+            <div className="card-image waves-effect waves-block waves-light">
+                <img className="activator" src={poster} />
+            </div>
+            <div className="card-content">
+                <span className="card-title activator grey-text text-darken-4">{title}<i className="material-icons right">{type}</i></span>
+            </div>
+
+        </div>
+    )
+}
+
+export { Movie }
--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,12 +1,27 @@
 // key = 2f4e2aa4
-import React from 'react'
+import React, { Component } from 'react'
+import { Movies } from '../components/Movies'
 
-function Main() {
-    return (
-        <main className='container film-content'>
-            Main
-        </main>
-    )
+class Main extends Component {
+    state = {
+        movies: []
+    }
+    componentDidMount() {
+        fetch('http://www.omdbapi.com/?apikey=2f4e2aa4&s=Matrix')
+            .then(response => response.json())
+            .then(data => this.setState({ movies: data.Search }))
+    }
+    render() {
+        const { movies } = this.state;
+
+        return (
+            <main className='container film-content'>
+
+                <Movies movies={this.state.movies} />
+            </main>
+        )
+
+    };
 }
 
 export { Main }
